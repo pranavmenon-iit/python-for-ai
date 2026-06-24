cd272c666b11c197e6f7243ddec385020e04b8d3
pranavmenon-iit <pranavmenoniit@gmail.com>
2026-06-24 18:16:20 +0530

    Refactor dog class definition and consolidate print statements in hello.py

--- hello.py
+++ hello.py
@@ -13,3 +13,18 @@ print(My_dog.breed)
 print(My_dog.name)
 print(My_dog.breed)
 
+class dog:
+    def __init__(self,name,breed):
+        self.name=name
+        self.breed=breed
+
+My_dog=dog("Rex","German Shepherd")
+print(My_dog.name)
+print(My_dog.breed)
+
+print(My_dog.name)
+print(My_dog.breed)
+
+print(My_dog.name)
+print(My_dog.breed)
+
